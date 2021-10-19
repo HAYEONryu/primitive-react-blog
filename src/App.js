a1ef4473205b12d5eb58714b04b15ccdd9bbeb29
HAYEONryu <21700470@handong.edu>
2021-10-19 19:55:17 +0900

    installing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route} from 'react-router-dom';
+import Postpage from './pages/Postpage';
+import Write from './pages/Write';
+import Register from './pages/Register';
+import LoginPage from './pages/Login';
+import PostList from './pages/PostList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = ()=>{
+  return(
+    <>
+      <Route component = {PostList} path={["/@:username","/"]} exact />
+      <Route component = {LoginPage} path="/login" />
+      <Route component = {Register} path="/register" />
+      <Route component = {Write} path="/write" />
+      <Route component = {Postpage} path="/@:username/:postId" />
+    </>
+    );
+};
 export default App;
